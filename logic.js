54604a41eaa7cd062bd9c8f7280fc9a3b4295785
Abhishek Gupta <abhishekg323@gmail.com>
2021-08-02 15:28:41 +0530

    Timer

---
+++ logic.js
@@ -0,0 +1,24 @@
+let n1=0,n2=0;
+let input = ()=>{
+    n1=parseFloat(document.getElementById('inp1').value)
+    n2=parseFloat(document.getElementById('inp2').value)
+}
+let output= n => {
+    document.getElementById('out').value=n;
+}
+document.getElementById('add').addEventListener('click',()=>{
+    input();
+    output(n1+n2);
+});
+document.getElementById('sub').addEventListener('click',()=>{
+    input();
+    output(n1-n2);
+});
+document.getElementById('mul').addEventListener('click',()=>{
+    input();
+    output(n1*n2);
+});
+document.getElementById('div').addEventListener('click',()=>{
+    input();
+    output(n1/n2);
+});
